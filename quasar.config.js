e983ee6e672692cc519cc0528a8f5a775dd31509
callebtc <93376500+callebtc@users.noreply.github.com>
2023-03-25 23:58:57 +0100

    router history mode

--- quasar.config.js
+++ quasar.config.js
@@ -44,7 +44,7 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-build
     build: {
-      vueRouterMode: "hash", // available values: 'hash', 'history'
+      vueRouterMode: "history", // available values: 'hash', 'history'
 
       // transpile: false,
       // publicPath: '/',
