d7bb6a4e6e91c6bb23b44b1ab31ecf6def4f3493
Sitt Guruvanich <aekazitt@gmail.com>
2023-04-29 13:38:28 +0700

    Support build mode with process.env in vite config

--- vite.config.js
+++ vite.config.js
@@ -9,54 +9,56 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [
-    VitePWA({
-      manifest: {
-        name: "Cashu",
-        short_name: "Cashu",
-        description: "Cashu Wallet",
-        display: "standalone",
-        orientation: "portrait",
-        background_color: "#12091f",
-        theme_color: "#12091f",
-        protocol_handlers: [
-          {
-            protocol: "web+cashu",
-            url: "/?token=%s",
+    process.env.BUILD_MODE == "pwa"
+      ? VitePWA({
+          manifest: {
+            name: "Cashu",
+            short_name: "Cashu",
+            description: "Cashu Wallet",
+            display: "standalone",
+            orientation: "portrait",
+            background_color: "#12091f",
+            theme_color: "#12091f",
+            protocol_handlers: [
+              {
+                protocol: "web+cashu",
+                url: "/?token=%s",
+              },
+              {
+                protocol: "web+lightning",
+                url: "/?lightning=%s",
+              },
+            ],
+            icons: [
+              {
+                src: "icons/icon-128x128.png",
+                sizes: "128x128",
+                type: "image/png",
+              },
+              {
+                src: "icons/icon-192x192.png",
+                sizes: "192x192",
+                type: "image/png",
+              },
+              {
+                src: "icons/icon-256x256.png",
+                sizes: "256x256",
+                type: "image/png",
+              },
+              {
+                src: "icons/icon-384x384.png",
+                sizes: "384x384",
+                type: "image/png",
+              },
+              {
+                src: "icons/icon-512x512.png",
+                sizes: "512x512",
+                type: "image/png",
+              },
+            ],
           },
-          {
-            protocol: "web+lightning",
-            url: "/?lightning=%s",
-          },
-        ],
-        icons: [
-          {
-            src: "icons/icon-128x128.png",
-            sizes: "128x128",
-            type: "image/png",
-          },
-          {
-            src: "icons/icon-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "icons/icon-256x256.png",
-            sizes: "256x256",
-            type: "image/png",
-          },
-          {
-            src: "icons/icon-384x384.png",
-            sizes: "384x384",
-            type: "image/png",
-          },
-          {
-            src: "icons/icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-    }),
+        })
+      : void 0,
     vue({
       template: transformAssetUrls,
     }),
