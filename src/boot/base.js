8d0ed184acbefcf09b5ecf54841341cfc8a8a80b
callebtc <93376500+callebtc@users.noreply.github.com>
2024-04-23 13:11:49 +0200

    default monochrome

--- src/boot/base.js
+++ src/boot/base.js
@@ -183,7 +183,7 @@ window.windowMixin = {
         this.$q.localStorage.getItem("cashu.theme")
       );
     } else {
-      this.changeColor("classic");
+      this.changeColor("monochrome");
     }
   },
 };
