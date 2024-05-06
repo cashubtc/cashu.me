dc31f0bbceaa155d442ecefa67c5a6771184fbc0
callebtc <93376500+callebtc@users.noreply.github.com>
2024-05-06 12:22:58 +0200

    add NWCDialog.vue

---
+++ src/components/NWCDialog.vue
@@ -0,0 +1,91 @@
+<template>
+  <q-dialog
+    v-model="showNWCDialog"
+    position="top"
+    backdrop-filter="blur(2px) brightness(60%)"
+  >
+    <q-card
+      v-if="showNWCData.connectionString"
+      class="q-px-lg q-pt-md q-pb-md qcard"
+    >
+      <div class="text-center q-mb-md q-mt-none q-pt-none">
+        <q-responsive :ratio="1" class="q-mx-md q-mt-none q-pt-none">
+          <vue-qrcode
+            :value="showNWCData.connectionString"
+            :options="{ width: 340 }"
+            class="rounded-borders"
+          >
+          </vue-qrcode>
+        </q-responsive>
+        <div class="row justify-center">
+          <q-card-section class="q-pa-sm">
+            <div class="row justify-center">
+              <q-item-label overline class="q-mb-sm q-pt-md text-white">
+                P2PK Lock</q-item-label
+              >
+            </div>
+            <div class="row justify-center q-py-md">
+              <q-item-label style="font-size: 28px" class="text-weight-bold">
+                <q-icon
+                  name="lock"
+                  :color="showNWCData.used ? 'red' : 'green'"
+                  class="q-mr-sm"
+                  size="sm"
+                />
+              </q-item-label>
+            </div>
+            <div v-if="showNWCData.used" class="row justify-center q-pt-sm">
+              <q-item-label
+                caption
+                class="text-weight-light text-white"
+                style="font-size: 14px"
+                >Warning: You have already used this lock before.</q-item-label
+              >
+            </div>
+            <div v-else class="row justify-center q-pt-sm">
+              <q-item-label
+                caption
+                class="text-weight-light text-white"
+                style="font-size: 14px"
+                >Connect any application with your wallet.</q-item-label
+              >
+            </div>
+          </q-card-section>
+        </div>
+        <div class="row q-mt-lg">
+          <q-btn
+            class="q-mx-xs"
+            size="md"
+            flat
+            @click="copyText(showNWCData.connectionString)"
+            >Copy</q-btn
+          >
+          <q-btn v-close-popup flat color="grey" class="q-ml-auto">Close</q-btn>
+        </div>
+      </div>
+    </q-card>
+  </q-dialog>
+</template>
+<script>
+import { defineComponent } from "vue";
+import { mapActions, mapState, mapWritableState } from "pinia";
+import VueQrcode from "@chenfengyuan/vue-qrcode";
+
+import { useNWCStore } from "src/stores/nwc";
+
+export default defineComponent({
+  name: "NWCDialog",
+  mixins: [windowMixin],
+  components: {
+    VueQrcode,
+  },
+  data: function () {
+    return {};
+  },
+  computed: {
+    ...mapState(useNWCStore, ["showNWCData"]),
+    ...mapWritableState(useNWCStore, ["showNWCDialog"]),
+  },
+  methods: {},
+});
+</script>
