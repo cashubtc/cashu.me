0cf4138f12b04e08816e659d558fca55e3ddaa7c
callebtc <93376500+callebtc@users.noreply.github.com>
2024-05-09 22:01:53 +0200

    Detect if other tabs are running (#123)
    * detect if another tab is already running
    
    * detect if another tab is already running
    
    * register broadcast channel earlier

---
+++ src/pages/AlreadyRunning.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    class="fullscreen bg-dark text-white text-center q-pa-md flex flex-center"
+  >
+    <div>
+      <div class="text-h3">Nope.</div>
+      <div class="text-h5 q-ma-lg text-grey">
+        Another tab is already running. Close this tab and try again.
+      </div>
+      <q-btn
+        rounded
+        class="q-mt-md"
+        color="white"
+        text-color="black"
+        unelevated
+        to="/"
+        label="Retry"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "AlreadyRunning",
+});
+</script>
