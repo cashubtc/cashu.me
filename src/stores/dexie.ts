baf74292b98dec47e41b7137e54b78561b913e63
callebtc <93376500+callebtc@users.noreply.github.com>
2024-08-11 21:03:46 +0300

    migrate proofs to dexie

--- src/stores/dexie.ts
+++ src/stores/dexie.ts
@@ -1,5 +1,6 @@
 import { defineStore } from "pinia";
 import Dexie, { Table } from 'dexie';
+import { useLocalStorage } from "@vueuse/core";
 
 export interface Proof {
   id: string
@@ -27,9 +28,35 @@ export const db = new MySubClassedDexie();
 export const useDexieStore = defineStore("dexie", {
   state: () => ({
     db: db,
+    migratedToDexie: useLocalStorage<boolean>("cashu.dexie.migrated", false),
   }),
   getters: {
   },
   actions: {
+    migrateToDexie: function () {
+      if (this.migratedToDexie) {
+        return;
+      }
+      // get all proofs from localstorage "cashu.proofs" ad migrate them to Dexie db.proofs
+      const proofs = localStorage.getItem("cashu.proofs");
+      if (proofs) {
+        const parsedProofs = JSON.parse(proofs) as Proof[];
+        parsedProofs.forEach((proof) => {
+          this.db.proofs.add(proof);
+        });
+      }
+      // do the same with spentProofs
+      const spentProofs = localStorage.getItem("cashu.spentProofs");
+      if (spentProofs) {
+        const parsedSpentProofs = JSON.parse(spentProofs) as Proof[];
+        parsedSpentProofs.forEach((proof) => {
+          this.db.spentProofs.add(proof);
+        });
+      }
+      this.migratedToDexie = true;
+      // remove localstorage "cashu.proofs" and "cashu.spentProofs"
+      // localStorage.removeItem("cashu.proofs");
+      // localStorage.removeItem("cashu.spentProofs");
+    }
   },
 });
