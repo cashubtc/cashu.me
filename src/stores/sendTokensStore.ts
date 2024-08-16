67c13f5c930ba2235f51df156dbf1c9fed1e924d
callebtc <93376500+callebtc@users.noreply.github.com>
2024-08-16 15:50:42 +0200

    senddata amount null

--- src/stores/sendTokensStore.ts
+++ src/stores/sendTokensStore.ts
@@ -5,11 +5,17 @@ export const useSendTokensStore = defineStore("sendTokensStore", {
     showSendTokens: false,
     showLockInput: false,
     sendData: {
-      amount: 0,
+      amount: null,
       memo: "",
       tokens: "",
       tokensBase64: "",
       p2pkPubkey: "",
+    } as {
+      amount: number | null;
+      memo: string;
+      tokens: string;
+      tokensBase64: string;
+      p2pkPubkey: string;
     },
   }),
   actions: {},
