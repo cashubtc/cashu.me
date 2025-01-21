7c046a9307531a1f95aee0bae8340c8f8c3da2bc
mjkeaton <michaeljkeaton@protonmail.com>
2025-01-21 16:34:19 +0100

    chore: postfix custom currency value with currency code

--- src/boot/base.js
+++ src/boot/base.js
@@ -61,7 +61,7 @@ window.windowMixin = {
       if (currency == "eur") value = value / 100;
 
       if (currency.length !== 3) {
-        return new Intl.NumberFormat(window.LOCALE).format(value);
+        return new Intl.NumberFormat(window.LOCALE).format(value) + " " + currency.toUpperCase();
       } else {
         return new Intl.NumberFormat(window.LOCALE, {
           style: "currency",
