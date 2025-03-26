26a9897ef1066843c620b70eb39abf90433d0861
mjkeaton <michaeljkeaton@protonmail.com>
2025-03-26 16:08:54 +0100

    chore(i18n): use q-select component

--- src/boot/i18n.js
+++ src/boot/i18n.js
@@ -3,12 +3,11 @@ import { createI18n } from "vue-i18n";
 import messages from "src/i18n";
 
 export default boot(async ({ app }) => {
-  const i18n = createI18n({
-    locale: navigator.language || "en-US",
-    globalInjection: true,
-    messages,
-  });
-
-  // Set i18n instance on app
-  app.use(i18n);
+  app.use(
+    createI18n({
+      locale: navigator.language || "en-US",
+      globalInjection: true,
+      messages,
+    })
+  );
 });
