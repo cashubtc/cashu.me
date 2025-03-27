c6fffe3e04a134cdbdeb52c83588f5375bba327c
mjkeaton <michaeljkeaton@protonmail.com>
2025-03-27 18:01:42 +0100

    chore(i18n): restore.ts

--- src/boot/i18n.js
+++ src/boot/i18n.js
@@ -2,12 +2,12 @@ import { boot } from "quasar/wrappers";
 import { createI18n } from "vue-i18n";
 import messages from "src/i18n";
 
+export const i18n = createI18n({
+  locale: navigator.language || "en-US",
+  globalInjection: true,
+  messages,
+});
+
 export default boot(async ({ app }) => {
-  app.use(
-    createI18n({
-      locale: navigator.language || "en-US",
-      globalInjection: true,
-      messages,
-    })
-  );
+  app.use(i18n);
 });
