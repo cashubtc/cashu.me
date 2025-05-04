ba45cf786327f17dac3796d4ba9efffe035b3744
lollerfirst <lollerfirst@gmail.com>
2025-05-04 20:03:50 +0200

    add semicolumn

--- src/components/MultinutPicker.vue
+++ src/components/MultinutPicker.vue
@@ -185,7 +185,7 @@ export default defineComponent({
               activeUnit
             );
             const mintClass = new MintClass(mint);
-            const proofs = mintClass.unitProofs(activeUnit)
+            const proofs = mintClass.unitProofs(activeUnit);
             return this.melt(proofs, quote, mintWallet, true);
           })
         );
