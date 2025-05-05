d45cd697f400f131e582882f5e1f16cb0fff4e88
callebtc <93376500+callebtc@users.noreply.github.com>
2025-05-05 23:23:08 +0200

    seems unnecessary

--- src/main.js
+++ src/main.js
@@ -1,15 +1,6 @@
 import { createApp } from "vue";
-import { createI18n } from "vue-i18n";
 import App from "./App.vue";
 import registerIcons from "./icons";
 
-const i18n = createI18n({
-  // something vue-i18n options here ...
-});
-
 const app = createApp(App);
 registerIcons(app);
-// ... other app configurations
-
-app.use(i18n);
-app.mount("#app");
