93e0508df47275acd0bc84f63cf50a632b2d3042
ritty65 <contact@simulacraproductions.net>
2025-06-01 19:54:57 +0200

    feat: add InfoTooltip component

---
+++ src/components/InfoTooltip.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-icon name="info_outline" color="grey">
+    <q-tooltip>
+      <slot>{{ text }}</slot>
+    </q-tooltip>
+  </q-icon>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "InfoTooltip",
+  props: {
+    text: {
+      type: String,
+      default: "",
+    },
+  },
+});
+</script>
