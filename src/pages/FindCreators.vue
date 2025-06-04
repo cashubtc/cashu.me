ed33a478829b488106514506832c2a28807dfef3
ritty65 <contact@simulacraproductions.net>
2025-06-04 09:11:18 +0200

    Replace Find Creators page with static search

--- src/pages/FindCreators.vue
+++ src/pages/FindCreators.vue
@@ -1,22 +1,11 @@
 <template>
-  <div
-    :class="[$q.dark.isActive ? 'bg-dark text-white' : 'bg-white text-dark', 'text-center q-pa-md flex flex-center']"
-  >
-    <FindCreatorsView />
-    <SendTokenDialog />
-  </div>
+  <div></div>
 </template>
 
-<script>
-import { defineComponent } from "vue";
-import FindCreatorsView from "components/FindCreatorsView.vue";
-import SendTokenDialog from "components/SendTokenDialog.vue";
+<script setup>
+import { onMounted } from 'vue'
 
-export default defineComponent({
-  name: "FindCreatorsPage",
-  components: {
-    FindCreatorsView,
-    SendTokenDialog,
-  },
-});
+onMounted(() => {
+  window.location.href = '/find-creators.html'
+})
 </script>
