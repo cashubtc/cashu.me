2e89a9d2291c010d4f0fd6fc55ea23efed8445a7
ritty65 <contact@simulacraproductions.net>
2025-06-04 10:21:09 +0200

    Load new find creators page via iframe

--- src/pages/FindCreators.vue
+++ src/pages/FindCreators.vue
@@ -1,7 +1,30 @@
 <template>
-  <FindCreatorsView />
+  <div class="find-creators-wrapper">
+    <iframe
+      src="/find-creators.html"
+      class="find-creators-frame"
+      title="Find Creators"
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
-import FindCreatorsView from 'components/FindCreatorsView.vue'
+// The FindCreators page now loads a standalone HTML file that contains the
+// entire implementation for searching and displaying creators. We simply embed
+// that file in an iframe to keep it separate from the Quasar SPA while
+// retaining navigation via the router.
 </script>
+
+<style scoped>
+.find-creators-wrapper {
+  height: 100vh;
+  padding: 0;
+  margin: 0;
+}
+
+.find-creators-frame {
+  border: none;
+  width: 100%;
+  height: 100%;
+}
+</style>
