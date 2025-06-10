05135ddf24ff00373adc187e8ab9d2059167d4dd
ritty65 <contact@simulacraproductions.net>
2025-06-10 16:57:19 +0200

    Revert "Add token message formatting utilities"

--- test/vitest/__tests__/message-utils.spec.ts
+++ test/vitest/__tests__/message-utils.spec.ts
@@ -1,9 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import {
-  sanitizeMessage,
-  createFormattedTokenMessage,
-  parseFormattedTokenMessage,
-} from '../../../src/js/message-utils';
+import { sanitizeMessage } from '../../../src/js/message-utils';
 
 describe('sanitizeMessage', () => {
   it('removes non printable characters', () => {
@@ -14,17 +10,5 @@ describe('sanitizeMessage', () => {
   it('truncates to max length', () => {
     const result = sanitizeMessage('abcdef', 3);
     expect(result).toBe('abc');
-  });
-});
-
-describe('formatted token message', () => {
-  it('round trips data', () => {
-    const msg = createFormattedTokenMessage('tok', 2, 'm', 10);
-    const parsed = parseFormattedTokenMessage(msg);
-    expect(parsed).toEqual({ token: 'tok', amount: 2, memo: 'm', unlockTime: 10 });
-  });
-
-  it('returns null without delimiters', () => {
-    expect(parseFormattedTokenMessage('foo')).toBeNull();
   });
 });
