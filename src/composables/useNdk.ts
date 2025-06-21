670abd85f8f9e0a0c485bc0c3f7abf9841e776e3
ritty65 <contact@simulacraproductions.net>
2025-06-21 13:19:13 +0200

    feat(ndk): provide ndk promise and handle boot errors

--- src/composables/useNdk.ts
+++ src/composables/useNdk.ts
@@ -1,11 +1,10 @@
-import { getCurrentInstance } from 'vue'
+import { inject } from 'vue'
 import type NDK from '@nostr-dev-kit/ndk'
 import { NdkBootError } from 'boot/ndk'
 import { useNdkBootStore } from 'src/stores/ndkBoot'
 
 export function useNdk(): Promise<NDK> {
-  const vm = getCurrentInstance()?.proxy as any
-  const ndkPromise: Promise<NDK> | undefined = vm?.$ndkPromise
+  const ndkPromise = inject<Promise<NDK> | undefined>('$ndkPromise')
   const store = useNdkBootStore()
   if (!ndkPromise) {
     const err = new NdkBootError('unknown')
