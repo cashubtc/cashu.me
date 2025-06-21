b8a0f19a7c8c12a8f296c5146ced09805469f5b0
ritty65 <contact@simulacraproductions.net>
2025-06-21 13:19:41 +0200

    feat(ndk): provide ndk promise and handle boot errors

---
+++ src/stores/bootError.ts
@@ -0,0 +1,16 @@
+import { defineStore } from 'pinia'
+import type { NdkBootError } from 'boot/ndk'
+
+export const useBootErrorStore = defineStore('bootError', {
+  state: () => ({
+    error: null as NdkBootError | null
+  }),
+  actions: {
+    set(error: NdkBootError) {
+      this.error = error
+    },
+    clear() {
+      this.error = null
+    }
+  }
+})
