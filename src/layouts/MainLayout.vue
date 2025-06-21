0210cbe7e0f644431373f92745933be2fa19f663
ritty65 <contact@simulacraproductions.net>
2025-06-21 12:00:56 +0200

    feat: handle ndk boot errors

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,12 +4,14 @@
     <q-page-container>
       <router-view />
     </q-page-container>
+    <NdkErrorDialog />
   </q-layout>
 </template>
 
 <script>
 import { defineComponent, ref } from "vue";
 import MainHeader from "components/MainHeader.vue";
+import NdkErrorDialog from "components/NdkErrorDialog.vue";
 import { useNostrStore } from "src/stores/nostr";
 import { useNutzapStore } from "src/stores/nutzap";
 
@@ -18,6 +20,7 @@ export default defineComponent({
   mixins: [windowMixin],
   components: {
     MainHeader,
+    NdkErrorDialog,
   },
   async mounted() {
     const myHex = useNostrStore().pubkey;
---
+++ src/stores/ndkBoot.ts
@@ -0,0 +1,26 @@
+import { defineStore } from 'pinia'
+import { getNdk, NdkBootError } from 'boot/ndk'
+
+export type NdkBootState = {
+  error: NdkBootError | null
+}
+
+export const useNdkBootStore = defineStore('ndkBoot', {
+  state: (): NdkBootState => ({
+    error: null
+  }),
+  actions: {
+    setError(err: NdkBootError | null) {
+      this.error = err
+    },
+    async retry() {
+      try {
+        await getNdk()
+        this.error = null
+      } catch (e) {
+        this.error = e instanceof NdkBootError ? e : new NdkBootError('unknown', (e as any)?.message)
+        throw e
+      }
+    }
+  }
+})
