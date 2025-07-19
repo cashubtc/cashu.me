fbcae390f1d13bde44e462740d2b017c19a11881
ritty65 <contact@simulacraproductions.net>
2025-07-19 18:23:13 +0200

    feat: add relay manager dialog

---
+++ src/components/RelayManagerDialog.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-dialog v-model="show" ref="dialog">
+    <q-card class="q-pa-md" style="min-width: 300px">
+      <q-card-section class="text-h6">Relays</q-card-section>
+      <q-card-section>
+        <RelayManager />
+      </q-card-section>
+      <q-card-actions align="right">
+        <q-btn flat label="Close" color="primary" v-close-popup @click="hideDialog" />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import RelayManager from './RelayManager.vue';
+
+const show = ref(false);
+
+function showDialog() {
+  show.value = true;
+}
+
+function hideDialog() {
+  show.value = false;
+}
+
+defineExpose({ show: showDialog, hide: hideDialog });
+</script>
+
