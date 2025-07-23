9757a93e128bc7b9a57a3bfd2f4f2b690d877110
ritty65 <contact@simulacraproductions.net>
2025-07-23 09:07:26 +0200

    Use npm cashu-ts

--- src/lib/cashu-ts/src/utils/Constants.ts
+++
@@ -1,7 +0,0 @@
-/**
- * per protocol definition since tokenV3 tokens have a prefix and a version number
- */
-const TOKEN_VERSION = 'A';
-const TOKEN_PREFIX = 'cashu';
-
-export { TOKEN_VERSION, TOKEN_PREFIX };
