e943513f4e4e10ab3eb1559c53c8a674fe26acd9
ritty65 <contact@simulacraproductions.net>
2025-08-04 14:03:40 +0200

    feat: use vpn_key icon for Nostr Login

--- src/i18n/es-ES/index.ts
+++ src/i18n/es-ES/index.ts
@@ -1504,7 +1504,7 @@ export default {
       nostrLoginTitle: "Nostr Login",
       nostrLogin: {
         description: "Authenticate using your Nostr keys.",
-        icon: "login",
+        icon: "vpn_key",
       },
     },
     navigation: {
