ff5216d61812a4dfb14819fdfea8018f09a022ca
ritty65 <contact@simulacraproductions.net>
2025-08-06 22:19:16 +0200

    feat: add subscribers static page

---
+++ src/pages/CreatorSubscribers.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="creator-subscribers-wrapper">
+    <iframe
+      src="/creator-subscribers.html"
+      class="creator-subscribers-frame"
+      title="Creator Subscribers"
+    />
+  </div>
+</template>
+
+<script setup>
+// No additional logic required for static subscribers page
+</script>
+
+<style scoped>
+.creator-subscribers-wrapper {
+  width: 100%;
+  height: 100%;
+}
+.creator-subscribers-frame {
+  width: 100%;
+  height: 100%;
+  border: none;
+}
+</style>
