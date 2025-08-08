92a9ae69b50928cc6577324034463c9e4ea1fa35
Simu <Simulacraproductions1@gmail.com>
2025-08-08 09:53:45 +0200

    chore: cleanup backups/logs; ignore nested node_modules; harden .gitignore

--- src/lib/cashu-ts.bak.1754632785/test/_replace-native-fetch.ts
+++
@@ -1,8 +0,0 @@
-// nock doesn't support native fetch, and hence we need this polyfill.
-
-import fetch, { Headers, Request, Response } from 'node-fetch';
-
-(globalThis as any).fetch = fetch;
-(globalThis as any).Headers = Headers;
-(globalThis as any).Request = Request;
-(globalThis as any).Response = Response;
