615bda72c4ef9bdc540a92b4e9fc8f4f1d5d745f
Simu <Simulacraproductions1@gmail.com>
2025-08-08 09:23:24 +0200

    WIP: before polyfill install

---
+++ src/boot/node-globals.ts
@@ -0,0 +1,16 @@
+import { Buffer } from 'buffer'
+import process from 'process'
+
+declare global {
+  interface Window {
+    Buffer?: typeof Buffer
+    process?: typeof process
+    global?: any
+  }
+}
+
+if (typeof window !== 'undefined') {
+  window.global = window.global || window
+  window.Buffer = window.Buffer || Buffer
+  window.process = window.process || process
+}
