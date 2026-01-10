a7fd3a45cdee9b148e567a9a11ced3c78b540f89
callebtc <93376500+callebtc@users.noreply.github.com>
2026-01-10 17:41:28 +0700

    back to one hour

--- src/stores/wallet.ts
+++ src/stores/wallet.ts
@@ -196,11 +196,10 @@ export const useWalletStore = defineStore("wallet", {
       }
       // if updateKeysets is true and keysetsLastFetched is older than 1 hour, fetch the keysets for the mint
       const ONE_HOUR = 60 * 60 * 1000;
-      const TEN_SECONDS = 10 * 1000; // for testing
       const lastUpdated = storedMint.lastKeysetsUpdated
         ? new Date(storedMint.lastKeysetsUpdated).getTime()
         : 0;
-      const mintNeedsUpdate = updateKeysets && (lastUpdated < Date.now() - TEN_SECONDS);
+      const mintNeedsUpdate = updateKeysets && (lastUpdated < Date.now() - ONE_HOUR);
       if (mintNeedsUpdate) {
         console.log("updating mint info and keys for mint", storedMint.url);
         try {
