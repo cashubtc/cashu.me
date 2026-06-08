178c0b1100891c15e930822762248f8d4306c43c
callebtc <93376500+callebtc@users.noreply.github.com>
2026-06-08 16:33:01 +0200

    fix unit bug

--- src/stores/ui.ts
+++ src/stores/ui.ts
@@ -107,10 +107,18 @@ export const useUiStore = defineStore("ui", {
       if (currency == "msat") return this.fromMsat(value);
       if (currency == "usd") value = value / 100;
       if (currency == "eur") value = value / 100;
-      return new Intl.NumberFormat(navigator.language, {
-        style: "currency",
-        currency: currency,
-      }).format(value);
+      try {
+        return new Intl.NumberFormat(navigator.language, {
+          style: "currency",
+          currency: currency,
+        }).format(value);
+      } catch {
+        return (
+          new Intl.NumberFormat(navigator.language).format(value) +
+          " " +
+          String(currency)
+        );
+      }
       // + " " +
       // currency.toUpperCase()
     },
